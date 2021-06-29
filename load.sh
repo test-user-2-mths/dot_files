07db7cbd4309d56ad31bf9c81cebfa74cd50f791
Patrick Coxall <mr.coxall@mths.ca>
2021-06-28 22:05:12 -0400

    Update load.sh
    add in SwiftLint

--- load.sh
+++ load.sh
@@ -16,10 +16,12 @@ echo Load YouCompleteMe plugin for Vim
 sudo apt install vim-youcompleteme
 vim-addon-manager install youcompleteme
 
-# load programming software
+# load java programming software
 echo load Java
 sudo apt install default-jdk
+
 # you might need to get a newer version of swift
+# https://swift.org/download/
 echo load Swift
 sudo apt-get install -y clang libblocksruntime0 libcurl4-openssl-dev
 wget https://swift.org/builds/swift-5.4.1-release/ubuntu2004/swift-5.4.1-RELEASE/swift-5.4.1-RELEASE-ubuntu20.04.tar.gz
@@ -28,8 +30,15 @@ sudo mkdir /usr/bin/swift
 sudo cp -R ./swift-5.4.1-RELEASE-ubuntu20.04/usr/* /usr/bin/swift
 echo 'export PATH="${PATH}":/usr/bin/swift/bin' >> ~/.bashrc
 
-# Swift syntax highlighting for Vim
+# SwiftLint
+# https://github.com/realm/SwiftLint/releases
+wget https://github.com/realm/SwiftLint/releases/download/0.43.1/swiftlint_linux.zip
+unzip -n swiftlint_linux.zip
+sudo mkdir /usr/bin/swiftlint
+sudo cp ./swiftlint /usr/bin/swiftlint/
+export PATH="${PATH}":/usr/bin/swiftlint
 
+# Swift syntax highlighting for Vim
 # Original Source: http://wingsquare.com/blog/swift-script-syntax-highlighting-and-indentation-for-vim-text-editor/
 # Another helpful article: https://billyto.github.io/blog/swift-syntax-vim
 # More about Vim packages: http://vimcasts.org/episodes/packages/
