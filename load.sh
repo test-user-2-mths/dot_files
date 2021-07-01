9f80b925f73c4ac07eef29c3a8048229eee7b76b
Patrick Coxall <mr.coxall@mths.ca>
2021-07-01 08:50:24 -0400

    Update load.sh

--- load.sh
+++ load.sh
@@ -74,6 +74,10 @@ cp -r ./swift/utils/vim ~/.vim/pack/bundle/start/swift
 echo "--- Cleaning up, removing swift repo.."
 rm -rf ./swift/
 
+# load GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt install gh
 
 # change some BASH settings
 echo adding some BASH commands to .bashrc
