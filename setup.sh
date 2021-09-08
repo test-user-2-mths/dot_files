4e0bd0fd7175767f11aae8ddf368820b5461623f
Patrick Coxall <mr.coxall@mths.ca>
2021-09-07 21:41:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -91,24 +91,6 @@ cp -r ./swift/utils/vim ~/.vim/pack/bundle/start/swift
 # echo "--- Cleaning up, removing swift repo.."
 # rm -rf ./swift/
 
-# change some BASH settings
-echo adding some BASH commands to .bashrc
-echo "" >> ~/.bashrc
-echo "parse_git_branch() {" >> ~/.bashrc
-echo "      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'" >> ~/.bashrc
-echo "}" >> ~/.bashrc
-echo "" >> ~/.bashrc
-echo "PS1='\[\033[01;32m\]\u:\[\033[01;34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ '" >> ~/.bashrc
-echo "PROMPT_DIRTRIM=1" >> ~/.bashrc
-
-echo List helper commands
-echo ""
-echo "# some more ls aliases"
-echo "alias ls='ls --color=auto'" >> ~/.bashrc
-echo "alias ll='ls -alF'" >> ~/.bashrc
-echo "alias la='ls -A'" >> ~/.bashrc
-echo "alias l='ls -CF'" >> ~/.bashrc
-
 # update Git
 echo upgrade Git
 sudo add-apt-repository ppa:git-core/ppa
